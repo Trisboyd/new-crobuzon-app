b6a2a6f02ca3f14dae7d60e29057a36307c71b20
Tristan Boyd <trisboyd@gmail.com>
2022-03-10 21:42:55 -0600

    'updated iron council photo'

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { bookLinks } from '../utilities/constants';
+import iron from '../images/iron.jpg';
 
 function Books() {
     return (
@@ -7,18 +8,18 @@ function Books() {
             <h3 className="section-title">Learn more with these novels by China Mieville</h3>
             <ul className="books__cards">
                 <li className="books__book">
-                    <a className="books__link" href={bookLinks.perdido} target="_blank"> 
-                        <img className="books__image" src="https://neverwasmag.com/wp-content/uploads/2020/08/Perdido-Street-Station-cover.jpg" />
+                    <a className="books__link" href={bookLinks.perdido} target="_blank">
+                        <img className="books__image" src="https://neverwasmag.com/wp-content/uploads/2020/08/Perdido-Street-Station-cover.jpg" alt='book' />
                     </a>
                 </li>
                 <li className="books__book">
                     <a className="books__link" href={bookLinks.scar} target="_blank">
-                        <img className="books__image" src="https://nekoplz.com/wp-content/uploads/2020/02/the-scar-china-mieville.jpg" />
+                        <img className="books__image" src="https://nekoplz.com/wp-content/uploads/2020/02/the-scar-china-mieville.jpg" alt='book' />
                     </a>
                 </li>
                 <li className="books__book">
                     <a className="books__link" href={bookLinks.iron} target="_blank">
-                        <img className="books__image" src="https://i.ebayimg.com/images/g/MrUAAOSwGuJfGdpD/s-l640.jpg" />
+                        <img className="books__image" src={iron} alt='book' />
                     </a>
                 </li>
             </ul>

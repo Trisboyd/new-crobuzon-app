005cd2006f00ff8cdd55255a3e637227a1db2909
Tristan Boyd <trisboyd@gmail.com>
2022-05-05 14:26:19 -0600

    'converted place to typescript'

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import map from "../images/new_crobuzon_map.PNG";
-import Place from './Place';
+import { Place } from './Place';
 import { places } from '../utilities/constants';
 
 // the map contains a sourced map of the city and several overlay 'locations' which can be clicked to open a popup with details
@@ -11,12 +11,12 @@ function Map(props) {
         <section className="map__container" id="map">
             <img className="map" src={map} alt="map" />
             {places.map((place) => {
-                    return (
-                        <Place 
-                        place={place}  
-                        onPlaceClick={props.onPlaceClick}/>
-                    )
-                })}
+                return (
+                    <Place
+                        place={place}
+                        onPlaceClick={props.onPlaceClick} />
+                )
+            })}
         </section>
     )
 }
---
+++ src/components/Place.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+interface Location {
+    title: string | null,
+    link: string | null,
+    about: string | null,
+    class: string | null
+}
+
+interface Props {
+    place: Location,
+    onPlaceClick: (place: Location) => void;
+}
+
+// A small overlay on the map that contains details about the location in the form of data-attributes
+// once clicked, opens a popup with the details
+export const Place: React.FC<Props> = (props: Props) => {
+    const { place } = props;
+
+    function handlePlaceClick(event: React.MouseEvent) {
+        const target = event.target as HTMLElement
+        const placeInfo: Location = {
+            title: target.getAttribute("data-title"),
+            link: target.getAttribute("data-link"),
+            about: target.getAttribute("data-about"),
+            class: target.getAttribute("classname")
+        }
+        props.onPlaceClick(placeInfo);
+    }
+
+    return (
+        <>
+            <div
+                className={`map__location map__location_${place.class}`
+                }
+                data-title={place.title}
+                data-link={place.link}
+                data-about={place.about}
+                onClick={(e) => handlePlaceClick(e)} >
+            </div >
+        </>
+    )
+}

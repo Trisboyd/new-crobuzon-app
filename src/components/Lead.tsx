725f930225e951c0e591b8fc98d2c9aebb6b8305
Tristan Boyd <trisboyd@gmail.com>
2022-05-09 20:29:08 -0600

    'converted header, lead to tsx'

--- src/components/Lead.tsx
+++ src/components/Lead.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
-function Lead() {
+export const Lead: React.FC = () => {
     return (
         <section className="lead">
             <h1 className="section-title">Explore New Crobuzon</h1>
             <p className="lead__text">Click on an area of the map to learn more</p>
         </section>
     )
-}
-
-export default Lead;
+};
